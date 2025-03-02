6f6f09d7fdec7636cdad9392eadc239ccc735903
hung203 <97659194+hung203@users.noreply.github.com>
2025-03-02 23:01:16 +0700

    Update home.py

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ import streamlit as st
 # Tạo selectbox để chọn dự án
 option = st.sidebar.selectbox(
     "📌 Chọn một dự án để thực hiện:",
-    ["Phân tích Titanic", "MNIST"]
+    ["Phân tích Titanic", "Classification MNIST", "Clustering Algorithms MNIST", "PCA & t-SNE"]
 )
 
 # Hiển thị nội dung tương ứng với lựa chọn
@@ -12,8 +12,15 @@ if option == "Phân tích Titanic":
     with open("streamlit_titanic.py", "r", encoding="utf-8") as file:
         code = file.read()
         exec(code)
-
-elif option == "MNIST":
+elif option == "Classification MNIST":
     with open("streamlit_Mnits.py", "r", encoding="utf-8") as file:
+        code = file.read()
+        exec(code)
+elif option == "Clustering Algorithms MNIST":
+    with open("streamlit_cluter.py", "r", encoding="utf-8") as file:
+        code = file.read()
+        exec(code)
+elif option == "PCA & t-SNE":
+    with open("streamlit_PCA_MNITS.py", "r", encoding="utf-8") as file:
         code = file.read()
         exec(code)
