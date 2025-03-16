1d7400e4bb36781b6005000b5d42f63ff8dc6e59
hung203 <97659194+hung203@users.noreply.github.com>
2025-03-16 22:26:02 +0700

    Add files via upload

--- home.py
+++ home.py
@@ -1,11 +1,25 @@
 import importlib
 import streamlit as st
 
+# Khởi tạo session_state nếu chưa có
+if 'last_option' not in st.session_state:
+    st.session_state.last_option = None
+
 # Tạo selectbox để chọn dự án
 option = st.sidebar.selectbox(
     "📌 Chọn một dự án để thực hiện:",
-    ["Phân tích Titanic", "Classification MNIST", "Clustering Algorithms MNIST", "PCA & t-SNE"]
+    ["Phân tích Titanic", "Classification MNIST", "Clustering Algorithms MNIST", "PCA & t-SNE","Neural network"]
 )
+
+# Kiểm tra nếu option thay đổi
+if st.session_state.last_option != option:
+    # Xóa toàn bộ session state (trừ last_option)
+    keys_to_remove = [key for key in st.session_state.keys() if key != 'last_option']
+    for key in keys_to_remove:
+        del st.session_state[key]
+    
+    # Cập nhật last_option
+    st.session_state.last_option = option
 
 # Hiển thị nội dung tương ứng với lựa chọn
 if option == "Phân tích Titanic":
@@ -22,5 +36,9 @@ elif option == "Clustering Algorithms MNIST":
         exec(code)
 elif option == "PCA & t-SNE":
     with open("streamlit_PCA_MNITS.py", "r", encoding="utf-8") as file:
+        code = file.read()
+        exec(code)
+elif option == "Neural network":
+    with open("streamlit_neral.py", "r", encoding="utf-8") as file:
         code = file.read()
         exec(code)
